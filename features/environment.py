56a1617ba714376349832ef5ca30f523179a2b43
riacono91 <iacono.rita91@gmail.com>
2025-01-29 13:06:37 +0100

    add parameter --user-data-dir

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,16 @@
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def before_scenario(context, scenario):
+    # Crea una directory temporanea unica per ogni esecuzione
+    temp_dir = tempfile.mkdtemp()
+
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument(f"--user-data-dir={temp_dir}")  # Usa una directory unica per i dati utente
 
     # Usa webdriver-manager per ottenere automaticamente il ChromeDriver
     service = Service(ChromeDriverManager().install())
@@ -16,4 +21,3 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     if hasattr(context, 'driver'):
         context.driver.quit()
-
